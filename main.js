6ce0a33a21dd04961dbcc9e430c015125faef36e
Micael Jerry <hei.jerry.2@gmail.com>
2022-03-20 16:04:48 +0300

    VF

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+let tab = [1,3,5,7,8,9] ;
+let element = 1 ;
+let min = 0 ;
+let max = tab.length-1 ;
+
 function dichotomie(tab,element,min,max){
     if(min>max){
         return false ;
     }
-    let comp = Math.floor((min+max)/2) ;
+    let comp = Math.round((min+max)/2) ;
     if(tab[comp]==element){
         return true ;
     }
@@ -13,8 +18,4 @@ function dichotomie(tab,element,min,max){
         return dichotomie(tab,element,comp+1,max)
     }
 }
-
-
-let tab=[1, 3, 5, 7, 8, 9]
-let element = 6
-console.log(dichotomie(tab,element,0,5))
+console.log(dichotomie(tab,element,min,max)) ;
